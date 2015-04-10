2bc8c6e9ac4a3780164b4032e6b3482fbef223f5
Ubuntu <ubuntu@ip-172-31-28-55.ec2.internal>
2015-04-10 00:53:48 +0000

    Updated full_script and kafkaConsumerToCsv

--- kafkaConsumerToCsv.py
+++ kafkaConsumerToCsv.py
@@ -2,6 +2,8 @@ from kafka.client import KafkaClient
 from kafka.consumer import KafkaConsumer
 from kafka.producer import SimpleProducer
 import json
+import datetime
+
 
 client = KafkaClient("ip-172-31-28-55.ec2.internal:6667")
 consumer = KafkaConsumer("shm", metadata_broker_list=['ip-172-31-28-55.ec2.internal:6667'])
@@ -16,22 +18,29 @@ def parseData(data):
     sampling_freq = json_data['samplingFreq']
     sensor_id = json_data['sensorId']
     reading_type = json_data['readingType']
-    mags = json_data['fftMags'].tolist()
+    mags = json_data['fftMags']
     fft_size = json_data['fftSize']
     output = str(sampling_freq) + ", " + str(sensor_id) + ", " + str(reading_type) + ", " + str(fft_size) + ", "
     for x in mags:
-        output += x + ", "
+        output += str(x) + ", "
     return output[:-2]
 
 def writeData(outfile, parsed):
-    f.write(parsed + "\n")
+    outfile.write(parsed + "\n")
         
 def main():
-    f = open('json_class.csv', 'w')
-    while (True):
+    f = open('json_class3.csv', 'w')
+    start = datetime.datetime.now().time()
+    x = 0
+    while (x < 15):
         data = getData()
+        print(data)
         parsed=parseData(data)
+        print(parsed)
         writeData(f, parsed)
+        print(parsed)
+        x+=1
+        print(parsed)
     f.close()
 
 if(__name__ == "__main__"):
